e0ea58a9947a21b747d1177d2546e49d47b59a22
Jeff May <jeff.n.may@gmail.com>
2020-08-29 10:28:42 -0700

    Fixed up EvaluatorSpec

--- core/src/test/scala/com/rallyhealth/vapors/adt/EvaluatorSpec.scala
+++ core/src/test/scala/com/rallyhealth/vapors/adt/EvaluatorSpec.scala
@@ -12,11 +12,13 @@ class EvaluatorSpec extends AnyFreeSpec {
   private object Facts {
     val name = Fact("name", "Joe Schmoe")
     val age = Fact("age", 32)
+    val weight = Fact("weight", 150)
     val probs = Fact("probs", Probs(Map("weightloss" -> .8)))
 
     val all = List(
       name,
       age,
+      weight,
       probs
     )
   }
@@ -35,11 +37,24 @@ class EvaluatorSpec extends AnyFreeSpec {
 
   s"$it should filter to the first matching set of facts using the || operator" in {
     val result = evaluate(Facts.all) {
+      or(
+        has(Fact("age", 80)),
+        has(Facts.weight),
+        has(Facts.probs)
+      )
+    }
+    assertResult(List(Facts.weight)) {
+      result.matchingFacts
+    }
+  }
+
+  s"$it should filter to only facts with the specified type of value" in pendingUntilFixed {
+    val result = evaluate(Facts.all) {
       withType[Probs] {
-        hasValue(32)
+        hasValue(Facts.probs.value)
       }
     }
-    assertResult(List(Facts.age)) {
+    assertResult(List(Facts.probs)) {
       result.matchingFacts
     }
   }
